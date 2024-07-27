99407905902f191c97542b60cf262da78869b47f
Murilo <mbalves1@outlook.com>
2024-07-27 10:50:18 -0300

    [feat]: added class-transformer, class-validator, and moment-timezone dependencies; updated AppController and AppModule; removed AppService; and modified main.ts to use global filters and customize Date toJSON method.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Body,
+  Controller,
+  Logger,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
+import { CreateCategoryDto } from './dtos/create-category.dto';
 
-@Controller()
+@Controller('api/v1')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private logger = new Logger(AppController.name);
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  private clientAdminBackend: ClientProxy;
+
+  constructor() {
+    this.clientAdminBackend = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://user:......etapadanaoDesenvolvidaSenhaPortaeUrl'],
+        queue: 'admin-backend',
+      },
+    });
+  }
+
+  @Post('categorys')
+  @UsePipes(ValidationPipe)
+  async createCategory(@Body() createCategoryDto: CreateCategoryDto) {
+    return await this.clientAdminBackend.emit(
+      'create-category',
+      createCategoryDto,
+    );
   }
 }
